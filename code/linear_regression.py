7dc43ec2139da7b6dc17ca2c82894cd4c3a71949
Bhuwan Dhingra <bhuwandhingra@gmail.com>
2014-09-07 16:31:26 -0700

    Added location data to linear regression.

--- code/linear_regression.py
+++ code/linear_regression.py
@@ -8,22 +8,28 @@ from sklearn import linear_model
 data = numpy.genfromtxt('../data/spectra_princomp.csv', delimiter=',')
 target = numpy.genfromtxt('../data/target.csv', delimiter=',')
 test = numpy.genfromtxt('../data/test_spectra_princomp.csv', delimiter=',')
+train_loc = numpy.genfromtxt('../data/train_location.csv', delimiter=',')
+test_loc = numpy.genfromtxt('../data/test_location.csv', delimiter=',')
 test_pidn = []
 with open('../data/test_pidn.csv','rb') as csvfile:
 	datareader = csv.reader(csvfile)
 	for row in datareader:
 		test_pidn.append(row)
 
+# Concatenate
+train = numpy.concatenate((data,train_loc), axis=1)
+testd = numpy.concatenate((test,test_loc), axis=1)
+
 # Fit linear model
 regr = linear_model.LinearRegression()
-regr.fit(data,target)
+regr.fit(train,target)
 
 # Get test predictions
-test_prediction = regr.predict(test)
+test_prediction = regr.predict(testd)
 tpr = test_prediction.tolist()
 
 # Save to file with PIDN
-with open('../predictions/linear_regression.csv','wb') as csvfile:
+with open('../predictions/linear_regression_with_loc.csv','wb') as csvfile:
 	datawriter = csv.writer(csvfile, delimiter=',')
 	datawriter.writerow(['PIDN', 'Ca', 'P', 'pH', 'SOC', 'Sand'])
 	for i in range(0,len(test_prediction)):
--- code/pca.py
+++ code/pca.py
@@ -17,7 +17,7 @@ with open('../data/training.csv','rb') as csvfile:
 	next(datareader)
 	for row in datareader:
 		train_spectra.append([float(i) for i in row[1:3579]])
-		train_location.append([i for i in row[3579:3595]])
+		train_location.append([float(i) for i in row[3579:3594]])
 		train_Y.append([float(i) for i in row[3595:]])
 with open('../data/sorted_test.csv', 'rb') as csvfile:
 	datareader = csv.reader(csvfile, delimiter=',')
@@ -25,7 +25,7 @@ with open('../data/sorted_test.csv', 'rb') as csvfile:
 	for row in datareader:
 		test_pidn.append(row[0])
 		test_spectra.append([float(i) for i in row[1:3579]])
-		test_location.append([i for i in row[3579:]])
+		test_location.append([float(i) for i in row[3579:3594]])
 
 # Create predictor and target variables
 tr_spectra = np.array(train_spectra)
@@ -42,6 +42,14 @@ with open('../data/test_pidn.csv','wb') as csvfile:
 	datawriter = csv.writer(csvfile, delimiter=',')
 	for row in test_pidn:
 		datawriter.writerow([row])
+with open('../data/train_location.csv','wb') as csvfile:
+	datawriter = csv.writer(csvfile, delimiter=',')
+	for row in train_location:
+		datawriter.writerow(row)
+with open('../data/test_location.csv','wb') as csvfile:
+	datawriter = csv.writer(csvfile, delimiter=',')
+	for row in test_location:
+		datawriter.writerow(row)
 np.savetxt('../data/spectra_princomp.csv', pca.transform(tr_spectra), delimiter=',')
 np.savetxt('../data/target.csv', tr_Y, delimiter=',')
 np.savetxt('../data/test_spectra_princomp.csv', pca.transform(te_spectra), delimiter=',')
