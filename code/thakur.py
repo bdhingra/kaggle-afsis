a1a6c733216340f29fbc9cc83a7484660195e94e
Bhuwan Dhingra <bdhingra@qti.qualcomm.com>
2014-10-14 18:10:19 -0700

    predictions using PCA on beating benchmark script.

--- code/thakur.py
+++ code/thakur.py
@@ -3,17 +3,17 @@ import pandas as pd
 import numpy as np
 from sklearn import svm, cross_validation
 
-train = pd.read_csv('../data/spectra_princomp.csv')
-test = pd.read_csv('../data/test_spectra_princomp.csv')
-labels = pd.read_csv('../data/target.csv')
+train = pd.read_csv('../data/spectra_princomp.csv', header=None)
+test = pd.read_csv('../data/test_spectra_princomp.csv', header=None)
+labels = pd.read_csv('../data/target.csv', header=None)
 
-xtrain, xtest = np.array(train), np.array(test)
+xtrain, xtest, xlabels = np.array(train), np.array(test), np.array(labels)
 
 sup_vec = svm.SVR(C=10000.0, verbose = 2)
 
 preds = np.zeros((xtest.shape[0], 5))
 for i in range(5):
-    sup_vec.fit(xtrain, labels[:,i])
+    sup_vec.fit(xtrain, xlabels[:,i])
     preds[:,i] = sup_vec.predict(xtest).astype(float)
 
 sample = pd.read_csv('../data/sample_submission.csv')
@@ -23,4 +23,4 @@ sample['pH'] = preds[:,2]
 sample['SOC'] = preds[:,3]
 sample['Sand'] = preds[:,4]
 
-sample.to_csv('beating_benchmark.csv', index = False)
+sample.to_csv('../data/beating_benchmark.csv', index = False)
