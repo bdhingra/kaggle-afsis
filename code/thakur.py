b5b6d3530ecdaa48da054e8de881500fa7c3dd0c
Bhuwan Dhingra <bhuwandhingra@gmail.com>
2014-10-16 00:12:51 -0700

    Grid Search

--- code/thakur.py
+++ code/thakur.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 from sklearn import svm, cross_validation
+from sklearn.grid_search import GridSearchCV
 
 train = pd.read_csv('../data/spectra_princomp.csv', header=None)
 test = pd.read_csv('../data/test_spectra_princomp.csv', header=None)
@@ -9,18 +10,11 @@ labels = pd.read_csv('../data/target.csv', header=None)
 
 xtrain, xtest, xlabels = np.array(train), np.array(test), np.array(labels)
 
-sup_vec = svm.SVR(C=10000.0, verbose = 2)
+tuned_parameters = [{'gamma':[1e-5, 1e-3, 0.1, 1, 100, 10000], 'C':[0.001, 0.01, 1, 100, 10000]}]
 
-preds = np.zeros((xtest.shape[0], 5))
-for i in range(5):
-    sup_vec.fit(xtrain, xlabels[:,i])
-    preds[:,i] = sup_vec.predict(xtest).astype(float)
+clf = GridSearchCV(svm.SVR(C=1), tuned_parameters, cv = 10, scoring='mean_squared_error')
+clf.fit(xtrain, xlabels)
 
-sample = pd.read_csv('../data/sample_submission.csv')
-sample['Ca'] = preds[:,0]
-sample['P'] = preds[:,1]
-sample['pH'] = preds[:,2]
-sample['SOC'] = preds[:,3]
-sample['Sand'] = preds[:,4]
+print("Best params:\n")
+print(clf.best_estimator_)
 
-sample.to_csv('../data/beating_benchmark.csv', index = False)
