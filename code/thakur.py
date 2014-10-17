7a03cd5238c00fccb13967341a8ce708f8fde73b
Bhuwan Dhingra <bdhingra@qti.qualcomm.com>
2014-10-16 17:27:46 -0700

    Grid Search cont.

--- code/thakur.py
+++ code/thakur.py
@@ -10,11 +10,17 @@ labels = pd.read_csv('../data/target.csv', header=None)
 
 xtrain, xtest, xlabels = np.array(train), np.array(test), np.array(labels)
 
-tuned_parameters = [{'gamma':[1e-5, 1e-3, 0.1, 1, 100, 10000], 'C':[0.001, 0.01, 1, 100, 10000]}]
+tuned_parameters = [{'gamma':[0.005, 0.01, 0.05, 0.1, 0.5], 'C':[0.05, 0.1, 0.5, 1, 5, 10, 50]}]
 
-clf = GridSearchCV(svm.SVR(C=1), tuned_parameters, cv = 10, scoring='mean_squared_error')
-clf.fit(xtrain, xlabels)
+clf = GridSearchCV(svm.SVR(C=...), tuned_parameters, cv = 10, scoring='mean_squared_error', verbose=1)
+clf.fit(xtrain, xlabels[:,1])
 
 print("Best params:\n")
 print(clf.best_estimator_)
 
+print("Grid scores on development set:")
+print()
+for params, mean_score, scores in clf.grid_scores_:
+    print("%0.3f (+/-%0.03f) for %r"
+          % (mean_score, scores.std() / 2, params))
+print()
