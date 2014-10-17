2cbe7f39e4fd215ca1def7812ffeaad08de31256
Bhuwan Dhingra <bhuwandhingra@gmail.com>
2014-10-16 23:40:02 -0700

    End to end setup.

--- code/thakur.py
+++ code/thakur.py
@@ -4,23 +4,38 @@ import numpy as np
 from sklearn import svm, cross_validation
 from sklearn.grid_search import GridSearchCV
 
-train = pd.read_csv('../data/spectra_princomp.csv', header=None)
-test = pd.read_csv('../data/test_spectra_princomp.csv', header=None)
+train = pd.read_csv('../data/training.csv')
+trloc = pd.read_csv('../data/train_location.csv', header=None)
+test = pd.read_csv('../data/sorted_test.csv')
 labels = pd.read_csv('../data/target.csv', header=None)
 
-xtrain, xtest, xlabels = np.array(train), np.array(test), np.array(labels)
+train.drop(['Ca', 'P', 'pH', 'SOC', 'Sand', 'PIDN'], axis=1, inplace=True)
+test.drop('PIDN', axis=1, inplace=True)
 
-tuned_parameters = [{'gamma':[0.005, 0.01, 0.05, 0.1, 0.5], 'C':[0.05, 0.1, 0.5, 1, 5, 10, 50]}]
+xtrain, xtest, xlabels, xloc = np.array(train)[:,:3578], np.array(test)[:,:3578], np.array(labels), np.array(trloc)
+X = np.concatenate((xtrain, xloc), axis=1)
 
-clf = GridSearchCV(svm.SVR(C=...), tuned_parameters, cv = 10, scoring='mean_squared_error', verbose=1)
-clf.fit(xtrain, xlabels[:,1])
+#tuned_parameters = [{'kernel':['rbf'],'gamma':[0.0005, 0.001, 0.003, 0.005, 0.007, 0.01], 'C':[50, 1000]}]
+#
+#clf = GridSearchCV(svm.SVR(C=10000), tuned_parameters, cv = 10, scoring='mean_squared_error', verbose=1, n_jobs=4)
+#clf.fit(xtrain, xlabels[:,0])
+#
+#print("Best params:\n")
+#print(clf.best_estimator_)
+#
+#print("Grid scores on development set:")
+#print()
+#for params, mean_score, scores in clf.grid_scores_:
+#    print("%0.3f (+/-%0.03f) for %r"
+#          % (mean_score, scores.std() / 2, params))
+#print()
 
-print("Best params:\n")
-print(clf.best_estimator_)
+mdl = svm.SVR(C=1000, gamma=0.0005, verbose=1)
+mdl.fit(xtrain, xlabels[:,0])
+preds = mdl.predict(xtest).astype(float)
 
-print("Grid scores on development set:")
-print()
-for params, mean_score, scores in clf.grid_scores_:
-    print("%0.3f (+/-%0.03f) for %r"
-          % (mean_score, scores.std() / 2, params))
-print()
+sample = pd.read_csv('../data/sub_28sept_v3.csv')
+sample['Ca'] = preds
+
+sample.to_csv('../data/sub_17oct_Ca_optimized_v2.csv', index=False)
+
